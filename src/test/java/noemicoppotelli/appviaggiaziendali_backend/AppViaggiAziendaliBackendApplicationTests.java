41fccbfdf9b7abb519fe2b5bd84e102de76180ee
Noemi Coppotelli <liz.c2004@gmail.com>
2026-07-10 12:57:51 +0200

    Initial project setup: Spring Boot config, PostgreSQL connection, Cloudinary/Mailgun dependencies

---
+++ src/test/java/noemicoppotelli/appviaggiaziendali_backend/AppViaggiAziendaliBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package noemicoppotelli.appviaggiaziendali_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppViaggiAziendaliBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
